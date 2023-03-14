2722f2e66feb9b8432abe669ca47e4b7bf171476
DESKTOP-HE1Q46S\user2 <221230pm362@gmail.com>
2023-03-14 15:56:24 +0900

    js

--- jsRepo/05_객체.js
+++ jsRepo/05_객체.js
@@ -49,4 +49,70 @@ window.onload = function(){
 
     });
 
-};
+    //객체와 반복문
+    const btn03 = document.querySelector("#btn03");
+    btn03.addEventListener("click" , function(){
+        
+        const p = {
+            nick : "1dragon" ,
+            age : 20 ,
+            atk : 100 ,
+            def : 50
+        };
+
+        // console.log(p.nick);
+        // console.log(p.age);
+        // console.log(p.atk);
+        // console.log(p.def);
+
+        for(let k in p){
+            console.log(`${k} : ${p[k]}`);
+        }
+
+    });
+
+    //객체의 속성 추가 및 제거
+    const btn04 = document.querySelector("#btn04");
+    btn04.addEventListener("click" , function(){
+        
+        const p = {};
+
+        p.atk = 123;
+        p.def = 30;
+        p['speed'] = 777;
+        p.attack = () => {
+            console.log("공격!!!");
+        };
+
+        delete(p.speed);
+
+        console.log(p);
+        p.attack();
+
+    });
+
+    //객체 배열
+    const btn05 = document.querySelector("#btn05");
+    btn05.addEventListener("click" , function(){
+        
+        const arr = [];
+
+        const s1 = {name : '심원용' ,java : 100 ,sql : 100 ,};
+        const s2 = {name : '심투용' ,java : 222 ,sql : 222 ,};
+        const s3 = {name : '심삼용' ,java : 333 ,sql : 333 ,};
+
+        // arr[0] = s1;
+        // arr[1] = s2;
+        // arr[2] = s3;
+
+        arr.push({name : '심원용' ,java : 100 ,sql : 100 ,});
+        arr.push(s2);
+        arr.push(s3);
+
+        console.log(arr);
+    });
+
+
+    
+
+}; //onload function

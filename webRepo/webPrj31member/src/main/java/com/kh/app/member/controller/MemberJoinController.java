9b20456b80571e379e71a140d893a88bd2c9e9f2
DESKTOP-HE1Q46S\user2 <221230pm362@gmail.com>
2023-04-21 15:55:45 +0900

    DML , DQL 연습

---
+++ webRepo/webPrj31member/src/main/java/com/kh/app/member/controller/MemberJoinController.java
@@ -0,0 +1,6 @@
+package com.kh.app.member.controller;
+
+//아이디 , 비번, 닉네임 데이터 받아서 DB에 insert 하기
+public class MemberJoinController {
+	
+}
---
+++ webRepo/webPrj31member/src/main/java/com/kh/app/member/controller/MemberLoginController.java
@@ -0,0 +1,6 @@
+package com.kh.app.member.controller;
+
+//아이디 , 비번 받아서 로그인(세션에담기) 처리하기
+public class MemberLoginController {
+
+}

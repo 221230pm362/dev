be72daa9148048da1267039c2b50dc7936a47e28
DESKTOP-HE1Q46S\user2 <221230pm362@gmail.com>
2023-06-07 21:49:19 +0900

    마이바티스

---
+++ mybatisRepo/mybatisPrj01/src/main/java/com/kh/app/board/dao/BoardDao.java
@@ -0,0 +1,36 @@
+package com.kh.app.board.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.kh.app.board.vo.BoardVo;
+import com.kh.app.template.JDBCTemplate;
+
+public class BoardDao {
+
+	public List<BoardVo> getBoardList(SqlSession ss) throws Exception {
+		return ss.selectList("board.getBoardList");
+	}
+
+	public int write(SqlSession ss, BoardVo vo) {
+		return ss.insert("board.write" , vo);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
